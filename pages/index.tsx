ecc9810f10569e3ba0e99ed04cbd16d36094602e
KHK <ston0538@gmail.com>
2020-11-11 02:18:37 +0900

    [UPDATE] Done
    [notion](https://www.notion.so/khk0613/Done-3b84c86169bb490383538302c9082a0c)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,53 @@ import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 export default function Home() {
   const [image, setImage] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const connectFileInput = (e: React.FormEvent) => {
     e.preventDefault();
     fileInputRef.current?.click();
   };
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (image) {
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+    } else {
+    }
+  }, [image]);
   return (
     <div className={styles.container}>
       <form>
-        <button onClick={connectFileInput} data-testid="button-test">
-          Add Image
-        </button>
+        {preview ? (
+          <img
+            src={preview}
+            style={{ objectFit: "cover" }}
+            onClick={connectFileInput}
+          />
+        ) : (
+          <button onClick={connectFileInput} data-testid="button-test">
+            Add Image
+          </button>
+        )}
+
         <input
-          // onChange={(e) => setImage(URL.createObjectURL(e.target.files[0]))}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setImage(e.target.files[0].name)
-          }
           data-testid="input-test"
           type="file"
           style={{ display: "none" }}
+          accept="image/*"
           ref={fileInputRef}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const file = e.target.files![0];
+            if (file && file.type.substr(0, 5) === "image") {
+              setImage(file);
+            } else {
+              setImage(null);
+            }
+          }}
         />
       </form>
-      <div data-testid="result">{image}</div>
     </div>
   );
 }
