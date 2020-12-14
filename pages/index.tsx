0190fa2f62cb0905e1ce004e114640c2336396f3
KHK <ston0538@gmail.com>
2020-12-15 01:45:33 +0900

    Done dropzone with react-hooks-form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,74 @@
-import { useEffect, useRef, useState, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 import styles from "../styles/Home.module.css";
 import { useDropzone } from "react-dropzone";
+import { useForm } from "react-hook-form";
+import { type } from "os";
 export default function Home() {
-  const [image, setImage] = useState<File | null>(null);
   const [preview, setPreview] = useState<string | null>(null);
+  const {
+    register,
+    unregister,
+    setValue,
+    handleSubmit,
+    getValues,
+    watch,
+  } = useForm();
   const onDrop = useCallback((acceptedFiles) => {
-    setImage(acceptedFiles[0]);
+    setValue("image", acceptedFiles[0], {
+      shouldValidate: false,
+      shouldDirty: true,
+    });
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   useEffect(() => {
-    if (image) {
+    register("image");
+  }, [register]);
+
+  const watchImage = watch("image");
+  useEffect(() => {
+    console.log(watchImage);
+    if (watchImage !== undefined) {
       const reader = new FileReader();
-      reader.readAsDataURL(image);
+      reader.readAsDataURL(watchImage);
       reader.onloadend = () => {
         setPreview(reader.result as string);
         console.log(reader.result);
       };
     }
-  }, [image]);
+  }, [watchImage]);
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
   return (
     <div>
-      <div
-        {...getRootProps()}
-        className={`${styles.dropzone} ${isDragActive ? styles.active : null}`}
-      >
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>Drag 'n' drop some files here, or click to select files</p>
-        )}
-      </div>
-      <div>
-        <input name="name" type="text" />
-      </div>
-      <div>
-        <p>image preview</p>
-        <img src={preview as string} alt="" style={{ objectFit: "cover" }} />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div
+          {...getRootProps()}
+          className={`${styles.dropzone} ${
+            isDragActive ? styles.active : null
+          }`}
+        >
+          <input name="image" {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          )}
+        </div>
+        <div>
+          <input
+            name="name"
+            type="text"
+            ref={register}
+            // onChange={(e) => setValue("name", e.currentTarget.value)}
+          />
+        </div>
+        <div>
+          <p>image preview</p>
+          <img src={preview as string} alt="" style={{ objectFit: "cover" }} />
+        </div>
+        <button>제출</button>
+      </form>
     </div>
   );
 }
