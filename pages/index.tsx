6ccf553491563a18372f981dbe1e52ddb1571dfa
KHK <ston0538@gmail.com>
2020-12-14 14:20:41 +0900

    Done dropzone

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,43 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import styles from "../styles/Home.module.css";
+import { useDropzone } from "react-dropzone";
 export default function Home() {
   const [image, setImage] = useState<File | null>(null);
   const [preview, setPreview] = useState<string | null>(null);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const connectFileInput = (e: React.FormEvent) => {
-    e.preventDefault();
-    fileInputRef.current?.click();
-  };
+  const onDrop = useCallback((acceptedFiles) => {
+    setImage(acceptedFiles[0]);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   useEffect(() => {
     if (image) {
       const reader = new FileReader();
       reader.readAsDataURL(image);
       reader.onloadend = () => {
         setPreview(reader.result as string);
+        console.log(reader.result);
       };
-    } else {
     }
   }, [image]);
   return (
-    <div className={styles.container}>
-      <form>
-        {preview ? (
-          <img
-            src={preview}
-            style={{ objectFit: "cover" }}
-            onClick={connectFileInput}
-          />
+    <div>
+      <div
+        {...getRootProps()}
+        className={`${styles.dropzone} ${isDragActive ? styles.active : null}`}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
         ) : (
-          <button onClick={connectFileInput} data-testid="button-test">
-            Add Image
-          </button>
+          <p>Drag 'n' drop some files here, or click to select files</p>
         )}
-
-        <input
-          data-testid="input-test"
-          type="file"
-          style={{ display: "none" }}
-          accept="image/*"
-          ref={fileInputRef}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const file = e.target.files![0];
-            if (file && file.type.substr(0, 5) === "image") {
-              setImage(file);
-            } else {
-              setImage(null);
-            }
-          }}
-        />
-      </form>
+      </div>
+      <div>
+        <input name="name" type="text" />
+      </div>
+      <div>
+        <p>image preview</p>
+        <img src={preview as string} alt="" style={{ objectFit: "cover" }} />
+      </div>
     </div>
   );
 }
